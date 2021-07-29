268dfeaea05fa1e3c822dc83a6ca985ff499b0d5
John Desrosiers <john.desro@gmail.com>
2021-07-29 08:19:00 -0700

    Remove boilerplate

--- src/example.js
+++
@@ -1,5 +0,0 @@
-function example() {
-  return "Hello, Example";
-}
-
-module.exports = example;
--- test/example.test.js
+++
@@ -1,7 +0,0 @@
-const example = require("../src/example");
-
-describe("example", () => {
-  test("returns 'Hello, Example'", () => {
-    expect(example()).toEqual("Hello, Example");
-  });
-});
